5888e307ea9a273fc78de2d3a1776e46b372683b
Mohammad Ashif <Mohammad Ashif>
2024-07-02 18:16:32 +0600

    porject setup done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+    return (
+        <>
+        <p>todo app</p>
+            <Button>Click me</Button>
+        </>
+    );
+}
+
+export default App;
